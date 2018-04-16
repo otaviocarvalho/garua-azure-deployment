9a366765ac32ba7e2952dbbd53fd766df2e7a63a
otaviocarvalho <ocarvalh@thoughtworks.com>
2018-04-15 21:42:25 -0300

    Adjust scripts to launch master/aggregator/edge nodes in specific locations

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 ### Garua Deploy - Azure tools to automate deployment
 
 - Create master deployment manually
-./create_deployment.sh -i "1236dd96-fdcf-4661-9cb4-0d973d4d7ee4" -g "iot-gppd-otavio-resource-group-runnable" -n "iot-gppd-deployment-master-auto-install" -f "../deployments/deployment_template_iot_master.json"
+./create_deployment.sh -i "1236dd96-fdcf-4661-9cb4-0d973d4d7ee4" -g "iot-gppd-otavio-resource-group-runnable" -n "iot-gppd-deployment-master-auto-install" -l "brazilsouth" -f "../deployments/deployment_template_iot_master.json"
 
 - Create edge nodes deployment manually
-./create_deployment.sh -i "1236dd96-fdcf-4661-9cb4-0d973d4d7ee4" -g "iot-gppd-otavio-resource-group-runnable" -n "iot-gppd-deployment-auto-install" -f "../deployments/deployment_template_iot_edge_multiple.json"
+./create_deployment.sh -i "1236dd96-fdcf-4661-9cb4-0d973d4d7ee4" -g "iot-gppd-otavio-resource-group-runnable" -n "iot-gppd-deployment-auto-install" -l "brazilsouth" -f "../deployments/deployment_template_iot_edge_multiple.json"
+
+- Create aggregator nodes deployment manually
+./create_deployment.sh -i "1236dd96-fdcf-4661-9cb4-0d973d4d7ee4" -g "iot-gppd-otavio-resource-group-runnable" -n "iot-gppd-deployment-auto-install-aggregator" -l "brazilsouth" -f "../deployments/deployment_template_iot_aggregator_multiple.json"
 
 - Run full deployment (still need to adjust our deployment template by the number of nodes manually)
 ./run_deployment.sh 2
--- scripts/create_deployment.sh
+++ scripts/create_deployment.sh
@@ -6,15 +6,16 @@ IFS=$'\n\t'
 # -o: prevents errors in a pipeline from being masked
 # IFS new value is less likely to cause confusing bugs when looping arrays or arguments (e.g. $@)
 
-usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -f <deploymentFile> " 1>&2; exit 1; }
+usage() { echo "Usage: $0 -i <subscriptionId> -g <resourceGroupName> -n <deploymentName> -f <deploymentFile> -l <location>" 1>&2; exit 1; }
 
 declare subscriptionId=""
 declare resourceGroupName=""
 declare deploymentName=""
 declare deploymentFile=""
+declare locationName=""
 
 # Initialize parameters specified from command line
-while getopts ":i:g:n:f:" arg; do
+while getopts ":i:g:n:f:l:" arg; do
 	case "${arg}" in
 		i)
 			subscriptionId=${OPTARG}
@@ -27,6 +28,10 @@ while getopts ":i:g:n:f:" arg; do
 			;;
 		f)
 			deploymentFile=${OPTARG}
+            ;;
+        l)
+			locationName=${OPTARG}
+
 		esac
 done
 shift $((OPTIND-1))
@@ -53,6 +58,12 @@ if [[ -z "$deploymentFile" ]]; then
 	echo "DeploymentFile:"
 	read deploymentFile
 fi
+
+if [[ -z "$locationName" ]]; then
+	echo "LocationName:"
+	read locationName
+fi
+
 
 #templateFile Path - template file to be used
 #templateFilePath="template.json"
@@ -97,7 +108,7 @@ echo "Creating resource group and vnet default with name" $resourceGroupName
 set -e
 (
     set -x
-    az group create -n $resourceGroupName --location brazilsouth 1> /dev/null
+    az group create -n $resourceGroupName --location $locationName 1> /dev/null
     az network vnet create --name default --resource-group $resourceGroupName --subnet-name default
 )
 
