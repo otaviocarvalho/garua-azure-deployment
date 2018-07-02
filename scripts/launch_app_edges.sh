508ff0b71261bf62cb5b3c7660bde012d94b1526
otaviocarvalho <ocarvalh@thoughtworks.com>
2018-07-01 21:29:55 -0300

    Modify default concurrency degree

--- scripts/launch_app_edges.sh
+++ scripts/launch_app_edges.sh
@@ -6,5 +6,5 @@
 
 parallel-ssh -h pssh_edge_hosts.txt -t 600 -p 1 -l ocarval -v -x "-o StrictHostKeyChecking=no -i credentials/id_rsa" \
         'bash -c "GOPATH=/home/ocarval/go nohup go run /home/ocarval/go/src/grpc_poc/iot/client/iot_client.go \
-            -c 1 -n 100000 -b 1 -s iot-gppd-otavio-auto-aggregator-1:50051 \
+            -c 100 -n 100000 -b 1 -s iot-gppd-otavio-auto-aggregator-1:50051 \
             < /dev/null >>/home/ocarval/script.out 2>>/home/ocarval/script.err &"'
