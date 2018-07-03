8ffd70a5a7234127dcfe39e48b4942def941e867
otaviocarvalho <ocarvalh@thoughtworks.com>
2018-07-03 18:59:24 -0300

    Adjust launcher variable name

--- scripts/launch_app_aggregator.sh
+++ scripts/launch_app_aggregator.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-declare aggregatorIp=$(head -1 ./pssh_master_hosts.txt)
+declare masterIp=$(head -1 ./pssh_master_hosts.txt)
 
 pssh -h pssh_aggregator_hosts.txt -t 600 -p 1 -l ocarval -v -x "-o StrictHostKeyChecking=no -i ../../credentials/id_rsa" \
         'bash -c "GOPATH=/home/ocarval/go nohup go run /home/ocarval/go/src/grpc_poc/iot/aggregator/iot_aggregator.go \
-            -c :50051 -s '$aggregatorIp':50052 \
+            -c :50051 -s '$masterIp':50052 \
             < /dev/null >>/home/ocarval/script.out 2>>/home/ocarval/script.err &"'
 
 #parallel-ssh -h pssh_aggregator_hosts.txt -t 600 -p 1 -l ocarval -v -x "-o StrictHostKeyChecking=no -i ../../credentials/id_rsa" \
         #'bash -c "GOPATH=/home/ocarval/go nohup go run /home/ocarval/go/src/grpc_poc/iot/aggregator/iot_aggregator.go \
-            #-c :50051 -s '$aggregatorIp':50052 \
+            #-c :50051 -s '$masterIp':50052 \
 #            < /dev/null >>/home/ocarval/script.out 2>>/home/ocarval/script.err &"'
