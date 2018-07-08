edbafb40946f4bf666b2479a56bf3bcd08bd7b8c
otaviocarvalho <ocarvalh@thoughtworks.com>
2018-07-08 11:37:15 -0300

    Adjust update script to support multiple regions

--- scripts/send_aggregator_vms_dependencies.sh
+++ scripts/send_aggregator_vms_dependencies.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-declare ipsAggregatorVms="./pssh_aggregator_hosts.txt"
+if [[ -z "$1" ]]; then
+    echo "Region is empty, please provide it. >> ./send_aggregator_vms_dependencies.sh <region>"
+else
+    declare ipsAggregatorVms="./pssh_aggregator_hosts_$1.txt"
 
-for ip in $(cat < "$ipsAggregatorVms");
-do
-    scp -r -i ../../credentials/id_rsa ./pssh_edge_hosts.txt ocarval@$ip:~
-    scp -r -i ../../credentials/id_rsa ../../credentials ocarval@$ip:~
-done
+    for ip in $(cat < "$ipsAggregatorVms");
+    do
+        scp -r -i ../../credentials/id_rsa ./pssh_edge_hosts_$1.txt ocarval@$ip:~
+        scp -r -i ../../credentials/id_rsa ../../credentials ocarval@$ip:~
+    done
+fi
--- scripts/send_master_vm_dependencies.sh
+++ scripts/send_master_vm_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-declare masterIp=$(head -1 ./pssh_master_hosts.txt)
-scp -r -i ../../credentials/id_rsa ../../credentials ocarval@$masterIp:~
+
+if [[ -z "$1" ]]; then
+    echo "Region is empty, please provide it. >> ./send_master_vm_dependencies.sh <region>"
+else
+    declare masterIp=$(head -1 ./pssh_master_hosts_$1.txt)
+    scp -r -i ../../credentials/id_rsa ../../credentials ocarval@$masterIp:~
+fi
