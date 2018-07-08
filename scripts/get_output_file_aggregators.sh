1d227d9fc5ae2e55dfe9f10d8a5c37f1e3820ecb
otaviocarvalho <ocarvalh@thoughtworks.com>
2018-07-08 14:08:42 -0300

    Get output files

--- scripts/get_output_file_aggregators.sh
+++ scripts/get_output_file_aggregators.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+if [[ -z "$1" ]]; then
+    echo "Region is empty, please provide it. >> ./get_output_file_aggregators.sh <region>"
+else
+    # Get output files from each aggregator machines
+    mkdir -p ./output_stats_aggregators_$1
+    i=1
+    while read p; do
+        mkdir -p ./output_stats_aggregators_$1/output_stats_aggregator_$i
 
-# Get output files from each aggregator machines
-mkdir -p ./output_stats_aggregators
-i=1
-while read p; do
-    mkdir -p ./output_stats_aggregators/output_stats_aggregator_$i
+        scp -i ../../credentials/id_rsa ocarval@$p:~/output_stats_aggregator.json ./output_stats_aggregators_$1/output_stats_aggregator_$i/output_stats_aggregator_$i.json
 
-    scp -i ../../credentials/id_rsa ocarval@$p:~/output_stats_aggregator.json ./output_stats_aggregators/output_stats_aggregator_$i/output_stats_aggregator_$i.json
+        # Get edge stats output folder at each aggregator machine
+        scp -i ../../credentials/id_rsa -r ocarval@$p:~/output_stats_edges_$1 ./output_stats_aggregators_$1/output_stats_aggregator_$i/output_stats_edges
 
-    # Get edge stats output folder at each aggregator machine
-    scp -i ../../credentials/id_rsa -r ocarval@$p:~/output_stats_edges ./output_stats_aggregators/output_stats_aggregator_$i/output_stats_edges
-
-    ((i++))
-done < ./pssh_aggregator_hosts.txt
+        ((i++))
+    done < ./pssh_aggregator_hosts_$1.txt
+fi
--- scripts/get_output_file_edges.sh
+++ scripts/get_output_file_edges.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-mkdir -p ~/output_stats_edges
-i=1
-while read p; do
-    scp -i ~/credentials/id_rsa ocarval@$p:~/output_stats_client.json ~/output_stats_edges/output_stats_edge_$i.json
-    ((i++))
-done < ./pssh_edge_hosts.txt
+if [[ -z "$1" ]]; then
+    echo "Region is empty, please provide it. >> ./get_output_file_edges.sh <region>"
+else
+    mkdir -p ~/output_stats_edges_$1
+    i=1
+    while read p; do
+        scp -i ~/credentials/id_rsa ocarval@$p:~/output_stats_client.json ~/output_stats_edges_$1/output_stats_edge_$i.json
+        ((i++))
+    done < ./pssh_edge_hosts_$1.txt
+fi
